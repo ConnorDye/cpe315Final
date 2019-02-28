bde9370d5139589f774596656ea96fca29cded79
nabishop <35745821+nabishop@users.noreply.github.com>
2019-02-28 14:59:49 -0800

    Update execute.cpp

--- execute.cpp
+++ execute.cpp
@@ -320,7 +320,7 @@ void execute()
       break;
     case ALU_CMP:
       setCarryOverflow(rf[alu.instr.cmp.rdn], alu.instr.cmp.imm, OF_SUB);
-      setNegativeZero(rf[alu.instr.cmp.rdn]);
+      setNegativeZero(rf[alu.instr.cmp.rdn] - alu.instr.cmp.imm);
       break;
     case ALU_ADD8I:
       // needs stats and flags
